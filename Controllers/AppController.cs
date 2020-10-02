8615b4a7fe02db5ff03953f4e9241e46abf54c71
Thato Mokhemisa <thato@tshimologong.joburg>
2020-10-02 19:43:00 +0200

    Services & Bootstrap added

---
+++ Controllers/AppController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly INullMailService _mailService;
+
+        //Inject Services
+        public AppController(INullMailService mailService)
+        {
+            _mailService = mailService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        //Get data from user
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            //Validate user input
+            if (ModelState.IsValid)
+            {
+                //Send the email
+                _mailService.SendMessage("thatomokhemisa@gmail.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: { model.Message }");
+                ViewBag.UserMessage = "Email Sent.";
+                ModelState.Clear();
+            }
+            else
+            {
+                //Show the errors
+            }
+            return View();
+        }
+    }
+}
+
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,12 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<INullMailService, NullMailService>();
+
+            //Support for mail service
+            //-----
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,13 +30,35 @@ namespace DutchTreat
         {
 
             //Serves HTML directly - black page (change internal path to index page)
-            app.UseDefaultFiles();
+            //app.UseDefaultFiles();
+
+            //Display exception page - for developers
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //Error page
+                app.UseExceptionHandler("/error");
+            }
 
             //static files saved in wwwroot dir - treated as the root of the web server
             app.UseStaticFiles();
 
             //Use node modules
             app.UseNodeModules();
+
+            //Enable routing
+            app.UseRouting();
+
+            //Enable multiple subsystems - listen to individual endpoints
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute("Fallback",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "App", action = "Index" });
+            });
         }
     }
 }
